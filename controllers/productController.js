6df93a2feabde2fdddd09e5b0e14c11f724812b0
HaroonAli48 <haroonaliali.pro@gmail.com>
2025-05-09 20:34:43 +0500

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,39 +5,38 @@ import productModel from '../models/productModel.js';
 const addProduct = async(req,res)=>{
     try {
         
-const {name,description,price,category,subCategory,sizes,bestseller}=req.body;
+        const {name,description,price,category,subCategory,sizes,bestseller}=req.body;
 
-const image1 = req.files.image1 && req.files.image1[0];
-const image2 = req.files.image2 && req.files.image2[0];
-const image3 = req.files.image3 && req.files.image3[0];
-const image4 = req.files.image4 && req.files.image4[0];
+        const image1 = req.files.image1 && req.files.image1[0];
+        const image2 = req.files.image2 && req.files.image2[0];
+        const image3 = req.files.image3 && req.files.image3[0];
+        const image4 = req.files.image4 && req.files.image4[0];
 
-const images = [image1,image2,image3,image4].filter((item)=> item!== undefined);
+        const images = [image1,image2,image3,image4].filter((item)=> item!== undefined);
 
-let imagesUrl =await Promise.all(
-    images.map(async (item)=>{
-        let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
-        return result.secure_url;
-    })
-)
+        let imagesUrl =await Promise.all(
+            images.map(async (item)=>{
+                let result = await cloudinary.uploader.upload(item.path,{resource_type:'image'});
+                return result.secure_url;
+            }))
 
-const productData = {
-    name,
-    description,
-    category,
-    subCategory,
-    price: Number(price),
-    bestseller: bestseller === 'true',
-    sizes: JSON.parse(sizes),
-    image: imagesUrl,
-    date: Date.now()
-}
+        const productData = {
+            name,
+            description,
+            category,
+            subCategory,
+            price: Number(price),
+            bestseller: bestseller === 'true',
+            sizes: JSON.parse(sizes),
+            image: imagesUrl,
+            date: Date.now()
+        }
 
-const product = new productModel(productData);
+        const product = new productModel(productData);
 
-await product.save()
+        await product.save()
 
-res.json({message:'product Added',success:true})
+        res.json({message:'product Added',success:true})
 
 
     } catch (error) {
@@ -49,11 +48,8 @@ res.json({message:'product Added',success:true})
 const listProducts = async(req,res)=>{
 
     try {
-        
-
         const products = await productModel.find({});
         res.json({success:true,products})
-
     } catch (error) {
         console.log(error);
         res.json({success:false,message:error.message})
@@ -69,15 +65,11 @@ const updateStock =async(req,res)=>{
 
 const removeProduct = async(req,res)=>{
 try {
-    
-
     await productModel.findByIdAndDelete(req.body.id)
     res.json({success:true,message:"product removed"})
 } catch (error) {
-
     console.log(error);
     res.json({success:false,message:error.message})
-    
 }
 }
 
