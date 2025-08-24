d97e68b4aa71f614bce1e54897de9d9c620b7fa5
HaroonAli48 <haroonaliali.pro@gmail.com>
2025-08-24 15:16:14 +0500

    .

--- routes/cartRoute.js
+++ routes/cartRoute.js
@@ -1,13 +1,19 @@
-import express from 'express'
-import { addToCart , updateCart , getUserCart, addReview, getReviews } from '../controllers/cartController.js'
-import authUser from '../middleware/auth.js';
+import express from "express";
+import {
+  addToCart,
+  updateCart,
+  getUserCart,
+  addReview,
+  getReviews,
+} from "../controllers/cartController.js";
+import authUser from "../middleware/auth.js";
 
 const cartRouter = express.Router();
 
-cartRouter.post('/get', authUser, getUserCart)
-cartRouter.post('/add', authUser, addToCart)
-cartRouter.post('/update', authUser, updateCart)
-cartRouter.post('/addReview', authUser, addReview)
-cartRouter.post('/getReviews',getReviews)
+cartRouter.post("/get", authUser, getUserCart);
+cartRouter.post("/add", authUser, addToCart);
+cartRouter.post("/update", authUser, updateCart);
+cartRouter.post("/addReview", authUser, addReview);
+cartRouter.post("/getReviews", getReviews);
 
 export default cartRouter;
